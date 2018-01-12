b622deda1eab9d096fd24d0fff3b20e52ae9d602
Matthew Kitt <mk@mkitt.net>
2018-01-12 11:00:55 -0700

    Dependency updates, refined rules and version bump

--- index.js
+++ index.js
@@ -14,24 +14,8 @@ module.exports = {
     node: true
   },
   rules: {
-    "semi": 0,
-    'no-alert': 2,
-    'no-console': 2,
-    "react/jsx-filename-extension": 0,
-    'react/sort-comp': [
-      'error',
-      {
-        order: [
-          'type-annotations',
-          'static-methods',
-          'lifecycle',
-          '/^on.+$/',
-          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-          'everything-else',
-          '/^render.+$/',
-          'render'
-        ]
-      }
-    ]
+    'object-curly-newline': 'off',
+    'react/jsx-filename-extension': 'off',
+    semi: ['error', 'never']
   }
 }
